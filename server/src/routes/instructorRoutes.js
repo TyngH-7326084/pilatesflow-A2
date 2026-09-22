836eee2b5ba3e134e0ead2ea5e376519157549e3
Tee-H123 <tyng.huang@connect.qut.edu.au>
2026-09-22 20:17:08 +1000

    PF-15: Build availability add/remove endpoint

--- server/src/routes/instructorRoutes.js
+++ server/src/routes/instructorRoutes.js
@@ -7,6 +7,8 @@ const {
   getInstructorById,
   updateInstructor,
   deleteInstructor,
+  addAvailability,
+  removeAvailability,
 } = require('../controllers/instructorController');
 
 const { requireAuth, requireAdmin } = require('../middleware/authMiddleware');
@@ -20,5 +22,11 @@ router.route('/:id')
   .get(requireAuth, requireAdmin, asyncHandler(getInstructorById))
   .put(requireAuth, requireAdmin, asyncHandler(updateInstructor))
   .delete(requireAuth, requireAdmin, asyncHandler(deleteInstructor));
+
+router.route('/:id/availability')
+  .post(requireAuth, requireAdmin, asyncHandler(addAvailability));
+ 
+router.route('/:id/availability/:slotId')
+  .delete(requireAuth, requireAdmin, asyncHandler(removeAvailability));
 
 module.exports = router;
